18717f19a15788e69c431dbe6a43890f0ed950a3
tonysaha007 <tony.ez007@gmail.com>
2025-12-29 02:43:20 +0600

    initial push

---
+++ src/test/java/com/testNGverify/TestNGTest.java
@@ -0,0 +1,21 @@
+package com.testNGverify;
+
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestNGTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test() {
+		System.out.println("TestNG Test Successful");
+        Assert.assertTrue(true, "TestNG Test Successful");
+    }
+}
